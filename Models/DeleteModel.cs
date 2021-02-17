bdfd4c58ade0e5764888ddff080205a91f3f723a
DanielLekLXY <1902640@sit.singaporetech.edu.sg>
2021-02-18 05:56:07 +0800

    Daniel Delete structure update

---
+++ Models/DeleteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models.Delete
+{
+    public class DeleteModel
+    {
+        private DateTime lastActive { get; set; }
+        private string username { get; set; }
+        private string id { get; set; }
+    }
+
+}
