c1fa57ce9672565770d34c6106e17f4e00f143a0
Zhikaizk <1902620@sit.singaporetech.edu.sg>
2021-02-18 17:51:34 +0800

    After presentation

--- Models/testing.cs
+++ Models/testing.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 namespace Project.Models.Notification
 {
 
- public  partial class testing   {
+ public  class testing   {
      private IEmail email;
      public void on(){
          this.email= new Email();
+         //can only access to whatever was implemented in the interface.
+         // my Email have Subscribe and unSubscribe but IEmail don't allow others to access it.
          email.UpdateObserver();
+         
          //this is where another componment will use //this is an exmaple first
          //email.Subscribe(EmailNotification testing);
          
