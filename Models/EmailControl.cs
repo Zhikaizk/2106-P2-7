29df9fe8df5dbbf14749f0793b3d105f467661ea
Zhikaizk <1902620@sit.singaporetech.edu.sg>
2021-02-19 21:21:11 +0800

    D2 sub

--- Models/EmailControl.cs
+++ Models/EmailControl.cs
@@ -23,11 +23,15 @@ namespace Project.Models.Notification
         ///String emailaddresses this is for backup just in case got input needede
         private Boolean Softwareupdate(String emailcontenthere)
         {
-            testing testing = new testing();
-            testing.on();
+            // this is the part where people can link via interface with testing function 
+            // testing function acts as other people file in order to intertract with IEmail 
+             testing testing = new testing();
+             testing.on();
             EmailNotification S1 = new EmailNotification();  
             //here will have the whole list of emails when retrieved from database
+            //SelectEmails function here();
             //the emails can use subscribe and notifyobservers all at one go
+            // CreateEmails function here ();
             Email O1 = new Email("leongzhikaii@gmail.com",emailcontenthere);  
             Email O2 = new Email("gameboys96@live.com",emailcontenthere);  
             O1.Subscribe(S1);
