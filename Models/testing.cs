df889d672cffdf5622ff80ac7880b0d9bb1ffb3d
Zhikaizk <1902620@sit.singaporetech.edu.sg>
2021-02-19 20:50:48 +0800

    D2 source code submission

--- Models/testing.cs
+++ Models/testing.cs
@@ -10,12 +10,8 @@ namespace Project.Models.Notification
          //can only access to whatever was implemented in the interface.
          // my Email have Subscribe and unSubscribe but IEmail don't allow others to access it.
          email.UpdateObserver();
-         
          //this is where another componment will use //this is an exmaple first
-         //email.Subscribe(EmailNotification testing);
-         
-     }
-     
-
+         //email.Subscribe(EmailNotification testing);         
+     }     
  }
 }
