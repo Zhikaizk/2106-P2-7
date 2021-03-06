b5471c23e316d5f544bc2a6f8fa5781e74f28cfe
jialin <1998jialin@gmail.com>
2021-03-06 12:44:06 +0800

    update

--- Models/Feedback/FeedbackDbContext.cs
+++ Models/Feedback/FeedbackDbContext.cs
@@ -1,40 +1,40 @@
-using EFCoreMySQL.Models;  
-using Microsoft.EntityFrameworkCore;  
-using System;  
-using System.Collections.Generic;  
-using System.Linq;  
-using System.Threading.Tasks;  
+// using EFCoreMySQL.Models;  
+// using Microsoft.EntityFrameworkCore;  
+// using System;  
+// using System.Collections.Generic;  
+// using System.Linq;  
+// using System.Threading.Tasks;  
   
-namespace EFCoreMySQL.DBContexts  
-{  
-    public class MyDBContext : DbContext  
-    {  
-        public DbSet<FeedbackDB> UserGroups { get; set; }   
+// namespace EFCoreMySQL.DBContexts  
+// {  
+//     public class MyDBContext : DbContext  
+//     {  
+//         public DbSet<FeedbackDB> UserGroups { get; set; }   
   
-        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)  
-        {   
-        }  
+//         public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)  
+//         {   
+//         }  
   
-        protected override void OnModelCreating(ModelBuilder modelBuilder)  
-        {  
-            // Use Fluent API to configure  
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)  
+//         {  
+//             // Use Fluent API to configure  
   
-            // Map entities to tables  
-            modelBuilder.Entity<FeedbackDB>().ToTable("FeedbackDB");   
+//             // Map entities to tables  
+//             modelBuilder.Entity<FeedbackDB>().ToTable("FeedbackDB");   
   
-            // Configure Primary Keys  
-            modelBuilder.Entity<FeedbackDB>().HasKey(fDB => fDB.FeedbackId).HasName("FeedbackId"); 
+//             // Configure Primary Keys  
+//             modelBuilder.Entity<FeedbackDB>().HasKey(fDB => fDB.FeedbackId).HasName("FeedbackId"); 
   
-            // Configure indexes  
+//             // Configure indexes  
   
-            // Configure columns  
-            modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackId).HasColumnType("int").IsRequired();  
-            modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.HouseholdEmail).HasColumnType("varchar(100)").IsRequired();  
-            modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedabackType).HasColumnType("varchar(100)").IsRequired();  
-            modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackContent).HasColumnType("varchar(500)").IsRequired();  
-            modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackStatus).HasColumnType("varchar(100)").IsRequired();  
+//             // Configure columns  
+//             modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackId).HasColumnType("int").IsRequired();  
+//             modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.HouseholdEmail).HasColumnType("varchar(100)").IsRequired();  
+//             modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedabackType).HasColumnType("varchar(100)").IsRequired();  
+//             modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackContent).HasColumnType("varchar(500)").IsRequired();  
+//             modelBuilder.Entity<FeedbackDB>().Property(fDB => fDB.FeedbackStatus).HasColumnType("varchar(100)").IsRequired();  
   
-            // Configure relationships   
-        }  
-    }  
-}  
+//             // Configure relationships   
+//         }  
+//     }  
+// }  
