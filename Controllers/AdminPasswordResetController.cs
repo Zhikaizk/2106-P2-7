961a937694d23b1adb6276ef652b03da62ef605e
ShiEnChan <chanen97@gmail.com>
2021-04-01 19:51:11 +0800

    update on retrieving email details info
    update on retrieving email details info

---
+++ Controllers/AdminPasswordResetController.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project.Models;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Web;
+using Project.Models.PasswordReset;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Project.Models.Notification;
+
+
+//added new things to try to fetch data 
+using MySql.Data.MySqlClient;
+//ends here 
+
+namespace Project.Controllers
+{
+    public class AdminPasswordResetController : Controller
+    {
+        
+                //trying to retrieve data
+        MySqlConnection con = new MySqlConnection();
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataReader rdr;
+        List<PasswordResetModel> email = new List<PasswordResetModel>();
+        //ends here
+
+
+        //trying to retrieve url from the submit button
+        private string url;
+
+        private readonly ILogger<PasswordResetController> _logger;
+
+        public AdminPasswordResetController(ILogger<PasswordResetController> logger)
+        {
+            _logger = logger;
+
+            //trying to retrieve data
+            con.ConnectionString = "server=t2-6.cthtaqebwmpy.us-east-1.rds.amazonaws.com;user=root;database=zk;port=3306;password=qwerty123";
+            //ends here
+        }
+
+        //route after admin click on the send email
+
+        // public string url(string householdEmail)
+        // {
+        //     return HttpUtility.HtmlEncode("Email have been send out");
+        // }
+
+
+        //display household email
+
+
+        // [HttpGet]
+        public IActionResult AdminPasswordResetPage()
+        {
+
+//to display on the web
+ Console.WriteLine("lol");
+            find();
+            Console.WriteLine("eneter");
+            //Console.WriteLine("admin password page Endedhere");
+           // Console.WriteLine("mytestinghere"+householdEmailDetails);
+       return View(email);     
+        } 
+
+             [HttpPost]
+        public IActionResult AdminPasswordResetPage(string disabledInput)
+        {
+
+
+             Console.WriteLine(disabledInput);
+              Console.WriteLine("sdiofnewfn");
+//to display on the web
+
+            //find();
+            // Console.WriteLine(householdEmailDetails);
+           // Console.WriteLine("mytestinghere"+householdEmailDetails);
+       return View();     
+        } 
+           
+
+        //retrieve database item
+
+        //trying to retrieve data and display on the web
+
+        private void find()
+        {
+            if (email.Count > 0)
+            {
+                email.Clear();
+            }
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                 Console.WriteLine("JOJOJOJOJO");
+                cmd.CommandText = "SELECT passwordResetID,householdEmail FROM PasswordReset";
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    email.Add(new PasswordResetModel()
+                    {
+                        passwordResetID = rdr["passwordResetID"].ToString(),
+                        householdEmailDetails = rdr["householdEmail"].ToString()
+                       
+
+                    });
+                     
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Done.");
+        }
+        //ends here
+
+    
+    
+    //to send email
+        [HttpGet]
+        public void submitButton(String householdEmailDetails,String passwordResetId)
+        {
+            Console.WriteLine("testinghere"+householdEmailDetails);
+            EmailControl emailControl = new EmailControl(householdEmailDetails, passwordResetId);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Controllers/PasswordResetController.cs
+++ Controllers/PasswordResetController.cs
@@ -11,6 +11,8 @@ using Project.Models.PasswordReset;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Project.Models.Notification;
+
 
 //added new things to try to fetch data 
 using MySql.Data.MySqlClient;
@@ -21,42 +23,12 @@ namespace Project.Controllers
     public class PasswordResetController : Controller
     {
 
-        //trying to retrieve data
-        MySqlConnection con = new MySqlConnection();
-        MySqlCommand cmd = new MySqlCommand();
-        MySqlDataReader rdr;
-        List<PasswordResetModel> email = new List<PasswordResetModel>();
-        //ends here
-
-
         private readonly ILogger<PasswordResetController> _logger;
 
         public PasswordResetController(ILogger<PasswordResetController> logger)
         {
             _logger = logger;
-
-            //trying to retrieve data
-            con.ConnectionString = "server=t2-6.cthtaqebwmpy.us-east-1.rds.amazonaws.com;user=root;database=zk;port=3306;password=qwerty123";
-            //ends here
         }
-
-        //route after admin click on the send email
-
-        public string url(string householdEmail)
-        {
-            return HttpUtility.HtmlEncode("Email have been send out");
-
-        }
-
-        // update the password GET PasswordReset/requestPassword
-
-        // public ActionResult Edit(string? householdEmail){
-        //     if (householdEmail == null){
-        //         return RedirectToAction(nameof(PasswordReset));
-        //     }
-        //     return View();
-
-        // }
 
 
         //get the passwordreset
@@ -71,30 +43,13 @@ namespace Project.Controllers
             return View();
         }
 
-
         // GET: 
         [HttpGet]
         public ActionResult edit(String householdEmail)
         {
-            // PasswordResetContext db  = new PasswordResetContext();
-            // PasswordResetModel pwdreset = db.PasswordResetModel.Single(x=> x.householdEmail == householdEmail);
-            // return View(pwdreset);
             return View();
         }
 
-
-        // //update the household password
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public ActionResult Edit([Bind("resetPassword")]
-        //  PasswordResetModel passwordResetModel)
-        //  {
-        //     if (ModelState.IsValid){
-        //         passwordResetDataGateway.Update(passwordResetModel);
-        //         return RedirectToAction("resetPasswordPage");
-        //     }
-        //     return View(passwordResetModel);
-        // }
         public ActionResult edit(PasswordResetModel passwordResetModel)
         {
             if (ModelState.IsValid)
@@ -118,7 +73,7 @@ namespace Project.Controllers
             return View();
         }
 
-        //newly added
+  
 
         //check the credentials
         [HttpPost]
@@ -137,63 +92,7 @@ namespace Project.Controllers
             PasswordResetControl pw = new PasswordResetControl(householdEmail, newResetPassword, confirmResetPassword);
             return View();
         }
-
-        //display household email
-
-
-        [HttpGet]
-        public IActionResult adminPasswordResetPage(PasswordResetModel objPasswordResetModel, String householdEmail)
-        {
-
-//to retrieve through console
-            // String email = householdEmail;
-            // Console.WriteLine(email);
-            // PasswordResetControl pw = new PasswordResetControl();
-            // return View();
-
-//to display on the web
-            find();
-            return View(email);
-        }
-
-        //retrieve database item
-
-
-        //trying to retrieve data and display on the web
-
-        private void find()
-        {
-            if (email.Count > 0)
-            {
-                email.Clear();
-            }
-            try
-            {
-                con.Open();
-                cmd.Connection = con;
-                cmd.CommandText = "SELECT passwordResetID,householdEmail FROM PasswordReset";
-                rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    email.Add(new PasswordResetModel()
-                    {
-                        passwordResetID = rdr["passwordResetID"].ToString(),
-                        householdEmailDetails = rdr["householdEmail"].ToString()
-
-                    });
-                }
-                con.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            Console.WriteLine("Done.");
-        }
-        //ends here
-
-
-
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
